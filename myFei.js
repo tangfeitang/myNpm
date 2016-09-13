aceab43ed8756ed921d2d50e172f9f3b7aa70dd9
tangfeitang <1411644476@qq.com>
2016-09-13 19:13:36 +0800

    we

---
+++ myFei.js
@@ -0,0 +1,7 @@
+/**
+ * Created by aser1 on 2016/8/22.
+ */
+var myNpmfei1 = require("myNpmfei1");
+console.log('ty');
+console.log(myNpmfei1.sayHi());
+console.log(myNpmfei1.study());
---
+++ myNpm1.js
@@ -0,0 +1,12 @@
+/**
+ * Created by aser1 on 2016/8/22.
+ */
+function sayHi(){
+    return '你好，我哦是';
+}
+function study(){
+    return '我在学习';
+}
+//输出模块所包含的成员
+exports.sayHi = sayHi;
+exports.study = study;
